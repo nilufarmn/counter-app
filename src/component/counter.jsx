8a9be8d5fa2b4370826a0e7b825ec3c6e022ade4
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-03 10:55:29 -0800

    create RenderTags function

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -4,29 +4,19 @@ class Counter  extends Component {
         count:0,
         tags:['tag1','tag2','tag3']
     };
+    renderTags()
+    {
+        if(this.state.tags.lenght===0) return <p>there are no tags</p>
+        return this.state.tags.map(tag=><li key={tag}>{tag}</li>)
+    }
+
     render() { 
         return( 
        
         <React.Fragment>
-            <span  className = {this.getBadgeClasses()} >{this.formatCount()}</span>
-            <button className="btn btn-secondary btn-sm">increament</button>
-            <ul>
-{this.state.tags.map(tag=><li key={tag}>{tag}</li>)}
-            </ul>
+        {this.renderTags()}
          </React.Fragment>
             );
-    }
-    getBadgeClasses() {
-        let classes = "badge m-2 badge-";
-        classes += (this.state.count === 0) ? "warning" : "primary";
-        return classes;
-    }
-
-    formatCount(){
-
-    const {count}=this.state;
-        return count === 0 ? "Zero" : count;
-
     }
 }
  
