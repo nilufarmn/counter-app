7487334342dcb76a032a432ad24c178d85d20aff
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-03 12:13:37 -0800

    create hanle increament function and refrenced to it with onclick

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,22 +2,31 @@ import React, { Component } from 'react';
 class Counter  extends Component {
     state={
         count:0,
-        tags:[]
     };
-    renderTags()
+    handleIncreament()
     {
-        if(this.state.tags.lenght===0) return <p>there are no tags</p>;
-        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+        console.log("Increament clicked");
     }
-
     render() { 
         return( 
        
         <React.Fragment>
-            {this.state.tags.length===0 && 'please create tags!!!!!'}
-        {this.renderTags()}
+            <span  className = {this.getBadgeClasses()} >{this.formatCount()}</span>
+            <button onClick={this.handleIncreament} className="btn btn-secondary btn-sm">increament</button>
          </React.Fragment>
             );
+    }
+    getBadgeClasses() {
+        let classes = "badge m-2 badge-";
+        classes += (this.state.count === 0) ? "warning" : "primary";
+        return classes;
+    }
+
+    formatCount(){
+
+    const {count}=this.state;
+        return count === 0 ? "Zero" : count;
+
     }
 }
  
