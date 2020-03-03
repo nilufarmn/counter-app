5745b262222e61ebae01ff1fad5b158886b6e962
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-03 05:01:15 -0800

    Added counter data in status object in Counter class

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -1,11 +1,15 @@
 import React, { Component } from 'react';
 class Counter  extends Component {
+    state={
+        count:1
+    };
     render() { 
         return( 
+       
         <React.Fragment>
-            <h1>Hello World1</h1>
+            <span>{this.state.count}</span>
             <button>increament</button>
-            </React.Fragment>
+         </React.Fragment>
             );
     }
 }
