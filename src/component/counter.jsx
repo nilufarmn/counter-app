64f8f84538a6f93fb1382e16c888cd490de39a83
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-03 12:03:22 -0800

    Create Condition using && Operand

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -2,18 +2,19 @@ import React, { Component } from 'react';
 class Counter  extends Component {
     state={
         count:0,
-        tags:['tag1','tag2','tag3']
+        tags:[]
     };
     renderTags()
     {
-        if(this.state.tags.lenght===0) return <p>there are no tags</p>
-        return this.state.tags.map(tag=><li key={tag}>{tag}</li>)
+        if(this.state.tags.lenght===0) return <p>there are no tags</p>;
+        return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
     }
 
     render() { 
         return( 
        
         <React.Fragment>
+            {this.state.tags.length===0 && 'please create tags!!!!!'}
         {this.renderTags()}
          </React.Fragment>
             );
