c003d261d5cb5bca7e1173ac92d3524fd288dc46
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-04 02:23:17 -0800

    old counters.jsx removed and created new one So problem solved

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -1,20 +1,14 @@
 import React, { Component } from 'react';
-import Counter from "./counter";
-class Counters  extends Component {
-    state = { 
-        counters:[
-            {id:1,value:0},
-            {id:2,value:0},
-            {id:3,value:0},
-            {id:4,value:0}
-        ]
-     };
+import Counter from './counter'
+class Counters extends Component {
+    state = {  }
     render() { 
-        return (
-             <div>
-{this.state.counters.map(counters=><Counter key={counter.id} />)}
-
-        </div> );
+        return (<div>
+<Counter />
+<Counter />
+<Counter />
+<Counter />
+        </div>  );
     }
 }
  
