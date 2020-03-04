4d9ca83056710851f2fc3b0a68448e45c64d0da5
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-04 14:26:22 -0800

    deleted selected properties

--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -12,7 +12,7 @@ class Counters extends Component {
     render() { 
         return (
         <div>
-{this.state.counters.map(counter =><Counter key={counter.id} value={counter.value} selected={true}/>
+{this.state.counters.map(counter =><Counter key={counter.id} value={counter.value} />
 )}
         </div>  );
     }
