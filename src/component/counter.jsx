d2d823ae36da7d62cb83ab2b3e0d9f9b6a435acf
Niloofarminavand <niloofarminavand@gmail.com>
2020-03-05 04:40:06 -0800

    delete function suing passing data between parents and chiled

--- src/component/counter.jsx
+++ src/component/counter.jsx
@@ -17,12 +17,12 @@ class Counter  extends Component {
             <ul>
             <span  className = {this.getBadgeClasses()} >{this.formatCount()}</span>
             <button onClick={this.handleIncreament} className="btn btn-secondary btn-sm">increament</button>
-            <button onClick={this.props.onDelete} className={"btn btn-danger btn-sm m-2"}>Delete</button>
+            <button onClick={()=>this.props.onDelete(this.props.id)} className="btn btn-danger btn-sm m-2">Delete</button>
             </ul>
          </React.Fragment>
             );
     }
-    getBadgeClasses() {
+    getBadgeClasses() {  
         let classes = "badge m-2 badge-";
         classes += (this.state.count === 0) ? "warning" : "primary";
         return classes;
--- src/component/counters.jsx
+++ src/component/counters.jsx
@@ -9,15 +9,16 @@ class Counters extends Component {
             { id:4 , value:0 } 
         ]
     };
-    handleDelete = (counterId) =>
+    handleDelete = counterId =>
     {
-        console.log("Event handler called ", counterId)
-    }
+        const counters = this.state.counters.filter(c =>c.id !==counterId);
+        this.setState({counters});
+    };
     render() { 
         return (
         <div>
-{this.state.counters.map(counter =><Counter onDelete={this.handleDelete} key={counter.id} value={counter.value} />
-)}
+{this.state.counters.map(counter =>(<Counter onDelete={this.handleDelete} key={counter.id} value={counter.value}  id={counter.id}/>
+))}
         </div>  );
     }
 }
